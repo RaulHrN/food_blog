b3b2feb6b6d3969d200bfebd2526ab2c7edffdfa
raulhnr <raulnascimento.contato@gmail.com>
2025-09-11 18:44:46 -0300

    HeaderBackground added to Header | refactoring of css classes for Header components

--- components/pageHeader/pageHeader.js
+++ components/pageHeader/pageHeader.js
@@ -2,21 +2,25 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/assets/logo.png";
 import styles from "./pageHeader.module.css";
+import PageHeaderBackground from "@/components/pageHeaderBackground/pageHeaderBackground";
 
 export default function PageHeader() {
     return (
-        <header className={styles.page_header}>
-            <div className={styles.header_title}>
+        <>
+        <PageHeaderBackground />
+        <header className={styles["page-header"]}>
+            <div className={styles["header-title"]}>
             <Image src={Logo} alt="Food Blog Logo" width={100} height={100} />
                 <h1>Food Blog</h1>
             </div>
 
-            <nav className={styles.header_nav}>
+            <nav className={styles["header-nav"]}>
                 <Link href="/">Home</Link>
                 <Link href="/meals">Meals</Link>
                 <Link href="/meals/share">Share</Link>
                 <Link href="/community">Community</Link>
             </nav>
         </header>
+        </>
     )
 }
